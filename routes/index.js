6b9fe01008e08eef41f4cb781ded2eb09ff7432a
StrangeNoob <sipun63427452@gmail.com>
2020-02-27 18:17:34 +0530

    Add Pages To Router

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,35 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Xstasy' });
+});
+router.get("/register", function (req, res, next) {
+  res.render("register", {
+    err: req.query.err
+  });
+});
+
+/* GET login page. */
+router.get("/login", function (req, res, next) {
+  res.render("login");
+});
+
+// GET Event Page
+router.get('/events', function (req, res, next) {
+  res.render('events');
+});
+// GET Sponsers Page
+router.get('/sponsers', function (req, res, next) {
+  res.render('sponsers');
+});
+// GET Star Page
+router.get('/stars', function (req, res, next) {
+  res.render('star')
+});
+// GET Gallery Page
+router.get('/gallery', function (req, res, next) {
+  res.render('gallery')
 });
 
 module.exports = router;
